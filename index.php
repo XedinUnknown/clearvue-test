d0c267ef60781183712cc642692b7cc515a5d135
XedinUnknown <xedin.unknown@gmail.com>
2023-07-04 01:31:48 +0200

    Add OpenAPI validation and routing
    Routing was supposed to be much more modular, but Slim does not cater well for that, apparently. Nonetheless, requests defined in the OpenAPI spec will be registered and their i/o data validated against that spec - all automatically.
    
    The trade-off to this ad-hoc routing solution is that _only_ OpenAPI routes can exist in this application. Given that it's a REST API, I doubt this will prove insufficient any time soon.

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-use Clearvue\Test1\RouteInterface;
+use cebe\openapi\spec\OpenApi;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Slim\Factory\AppFactory;
+use Slim\Interfaces\RouteCollectorProxyInterface;
 
 (function (string $mainFilePath): void {
     $rootDir = dirname($mainFilePath);
@@ -27,21 +26,47 @@ use Slim\Factory\AppFactory;
     // Initialize app
     /** @var bool $isDebug */
     $isDebug = $appContainer->get('clearvue/test1/is_debug');
-
     $app = AppFactory::createFromContainer($appContainer);
     $app->addRoutingMiddleware();
     $app->addErrorMiddleware($isDebug, true, true);
 
-    /** @var array<callable(RequestInterface, RequestHandlerInterface): ResponseInterface> $appMiddleware */
-    $appMiddleware = $appContainer->get('clearvue/test1/app/middleware/list');
-    foreach ($appMiddleware as $middleware) {
-        $app->add($middleware);
-    }
+    /** @var OpenApi $apiSpec */
+    $apiSpec = $appContainer->get('clearvue/test1/api/spec');
+    /** @var MiddlewareInterface $apiValidationMiddleware */
+    $apiValidationMiddleware = $appContainer->get('clearvue/test1/api/validation_middleware');
 
-    /** @var iterable<RouteInterface> $appRoutes */
-    $appRoutes = $appContainer->get('clearvue/test1/app/routes/list');
-    foreach ($appRoutes as $route) {
-        $app->map($route->getMethods(), $route->getPath(), $route->getHandler());
+    // Register routes for all defined servers
+    foreach ($apiSpec->servers as $server) {
+        $apiUrl = parse_url($server->url);
+        $apiHost = $apiUrl['host'] ?? null;
+        $apiPath = $apiUrl['path'] ?? '/';
+        $currentHost = $_SERVER['HTTP_HOST'] ?? null;
+
+        // Only add routes for known servers or relative base URLs
+        if ($apiHost !== null && $apiHost === $currentHost) {
+            continue;
+        }
+
+        // Use server path to group routes
+        $app->group(
+            $apiPath,
+            function (RouteCollectorProxyInterface $group) use (
+                $apiSpec,
+                $apiValidationMiddleware,
+                $appContainer
+            ): void {
+                foreach ($apiSpec->paths as $key => $path) {
+                    foreach ($path->getOperations() as $method => $operation) {
+                        /** @var 'GET' | 'POST' $methodName */
+                        $methodName = strtoupper($method);
+                        $handler = $operation->operationId;
+
+                        $group->map([$methodName], $key, $handler)
+                            ->add($apiValidationMiddleware);
+                    }
+                }
+            }
+        );
     }
 
     $app->run();
--- src/factories.php
+++ src/factories.php
@@ -2,8 +2,15 @@
 
 declare(strict_types=1);
 
+use cebe\openapi\Reader;
+use cebe\openapi\spec\OpenApi;
+use Dhii\Services\Factories\StringService;
 use Dhii\Services\Factories\Value;
-use Dhii\Services\Service;
+use Dhii\Services\Factory;
+use League\OpenAPIValidation\PSR15\ValidationMiddlewareBuilder;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
 
 return function (string $mainFilePath): array {
     return [
@@ -14,7 +21,34 @@ return function (string $mainFilePath): array {
             return dirname($mainFilePath);
         }),
         'clearvue/test1/is_debug' => new Value(true),
-        'clearvue/test1/app/middleware/list' => new Value([]),
-        'clearvue/test1/app/routes/list' => Service::fromFile(__DIR__ . '/routes.php'),
+        'clearvue/test1/api/spec_path' => new StringService('{0}/openapi.yml', [
+            'clearvue/test1/root_path',
+        ]),
+        'clearvue/test1/api/spec' => new Factory([
+            'clearvue/test1/api/spec_path',
+        ], function (string $specPath): OpenApi {
+            return Reader::readFromYamlFile($specPath);
+        }),
+        'clearvue/test1/api/validation_middleware' => new Factory([
+            'clearvue/test1/api/spec',
+        ], function (OpenApi $spec): MiddlewareInterface {
+            $validationMiddleware = (new ValidationMiddlewareBuilder())
+                ->fromSchema($spec)
+                ->getValidationMiddleware();
+
+            return $validationMiddleware;
+        }),
+        'clearvue/test1/api/handlers/categories/list' => new Factory([
+        ], function () {
+            return function (RequestInterface $request, ResponseInterface $response): ResponseInterface {
+                $categories = [];
+
+                $response = $response->withAddedHeader('Content-Type', 'application/json');
+                $response->getBody()
+                    ->write((string) json_encode($categories));
+
+                return $response;
+            };
+        }),
     ];
 };
